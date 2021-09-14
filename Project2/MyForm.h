8b5e9e6afd4e29c40cbfa67a599dc53eaa849b7d
Георгий Овсянников <ovsyannikov.gosha@list.ru>
2021-09-14 18:44:28 +0700

    Добавлены вкладки

--- Project2/MyForm.h
+++ Project2/MyForm.h
@@ -34,6 +34,14 @@ namespace Project2 {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::TabControl^ TabControl;
+	protected:
+	private: System::Windows::Forms::TabPage^ tabPage1;
+	private: System::Windows::Forms::TabPage^ tabPage2;
+	private: System::Windows::Forms::TabPage^ tabPage3;
+	private: System::Windows::Forms::TabPage^ tabPage4;
+	private: System::Windows::Forms::TabPage^ tabPage5;
+	private: System::Windows::Forms::TabPage^ tabPage6;
 
 	private:
 		/// <summary>
@@ -48,15 +56,99 @@ namespace Project2 {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			this->TabControl = (gcnew System::Windows::Forms::TabControl());
+			this->tabPage1 = (gcnew System::Windows::Forms::TabPage());
+			this->tabPage2 = (gcnew System::Windows::Forms::TabPage());
+			this->tabPage3 = (gcnew System::Windows::Forms::TabPage());
+			this->tabPage4 = (gcnew System::Windows::Forms::TabPage());
+			this->tabPage5 = (gcnew System::Windows::Forms::TabPage());
+			this->tabPage6 = (gcnew System::Windows::Forms::TabPage());
+			this->TabControl->SuspendLayout();
 			this->SuspendLayout();
+			// 
+			// TabControl
+			// 
+			this->TabControl->Controls->Add(this->tabPage1);
+			this->TabControl->Controls->Add(this->tabPage2);
+			this->TabControl->Controls->Add(this->tabPage3);
+			this->TabControl->Controls->Add(this->tabPage4);
+			this->TabControl->Controls->Add(this->tabPage5);
+			this->TabControl->Controls->Add(this->tabPage6);
+			this->TabControl->Location = System::Drawing::Point(13, 5);
+			this->TabControl->Name = L"TabControl";
+			this->TabControl->SelectedIndex = 0;
+			this->TabControl->Size = System::Drawing::Size(822, 670);
+			this->TabControl->TabIndex = 0;
+			// 
+			// tabPage1
+			// 
+			this->tabPage1->Location = System::Drawing::Point(4, 22);
+			this->tabPage1->Name = L"tabPage1";
+			this->tabPage1->Padding = System::Windows::Forms::Padding(3);
+			this->tabPage1->Size = System::Drawing::Size(814, 644);
+			this->tabPage1->TabIndex = 0;
+			this->tabPage1->Text = L"Персонал";
+			this->tabPage1->UseVisualStyleBackColor = true;
+			// 
+			// tabPage2
+			// 
+			this->tabPage2->Location = System::Drawing::Point(4, 22);
+			this->tabPage2->Name = L"tabPage2";
+			this->tabPage2->Padding = System::Windows::Forms::Padding(3);
+			this->tabPage2->Size = System::Drawing::Size(814, 644);
+			this->tabPage2->TabIndex = 1;
+			this->tabPage2->Text = L"Пациент";
+			this->tabPage2->UseVisualStyleBackColor = true;
+			// 
+			// tabPage3
+			// 
+			this->tabPage3->Location = System::Drawing::Point(4, 22);
+			this->tabPage3->Name = L"tabPage3";
+			this->tabPage3->Padding = System::Windows::Forms::Padding(3);
+			this->tabPage3->Size = System::Drawing::Size(814, 644);
+			this->tabPage3->TabIndex = 2;
+			this->tabPage3->Text = L"Записи";
+			this->tabPage3->UseVisualStyleBackColor = true;
+			// 
+			// tabPage4
+			// 
+			this->tabPage4->Location = System::Drawing::Point(4, 22);
+			this->tabPage4->Name = L"tabPage4";
+			this->tabPage4->Padding = System::Windows::Forms::Padding(3);
+			this->tabPage4->Size = System::Drawing::Size(814, 607);
+			this->tabPage4->TabIndex = 3;
+			this->tabPage4->Text = L"Кабинеты";
+			this->tabPage4->UseVisualStyleBackColor = true;
+			// 
+			// tabPage5
+			// 
+			this->tabPage5->Location = System::Drawing::Point(4, 22);
+			this->tabPage5->Name = L"tabPage5";
+			this->tabPage5->Padding = System::Windows::Forms::Padding(3);
+			this->tabPage5->Size = System::Drawing::Size(814, 607);
+			this->tabPage5->TabIndex = 4;
+			this->tabPage5->Text = L"Услуги";
+			this->tabPage5->UseVisualStyleBackColor = true;
+			// 
+			// tabPage6
+			// 
+			this->tabPage6->Location = System::Drawing::Point(4, 22);
+			this->tabPage6->Name = L"tabPage6";
+			this->tabPage6->Padding = System::Windows::Forms::Padding(3);
+			this->tabPage6->Size = System::Drawing::Size(814, 607);
+			this->tabPage6->TabIndex = 5;
+			this->tabPage6->Text = L"Должности";
+			this->tabPage6->UseVisualStyleBackColor = true;
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(847, 505);
+			this->ClientSize = System::Drawing::Size(847, 687);
+			this->Controls->Add(this->TabControl);
 			this->Name = L"MyForm";
 			this->Text = L"MyForm";
+			this->TabControl->ResumeLayout(false);
 			this->ResumeLayout(false);
 
 		}
